fd3c240d1167f8b8ed5b6def343a77d423906a74
Kendrick Pham <newbiepro25@gmail.com>
2022-06-15 17:55:20 -0700

    mission 3 complete, mission impossible still needs questions

--- sorting/src/sorting/missionImpossible.java
+++ sorting/src/sorting/missionImpossible.java
@@ -2,7 +2,14 @@ package sorting;
 
 public class missionImpossible {
 	public static int maxTroops(laserTower[] towers) {
-		return 0;
+		int area = 0;
+		for(int i = 0; i < towers.length; i++) {
+			int temp =  towers[i].x * towers[i].y;
+			if(area < temp) {
+				area = temp;
+			}
+		}
+		return area;
 	}
 	
 	public static class laserTower{
@@ -13,6 +20,16 @@ public class missionImpossible {
 			this.x = x;
 			this.y = y;
 		}
+	}
+	
+	public static void main(String[] args) {
+		laserTower tower1 = new laserTower(3, 5); //15
+		laserTower tower2 = new laserTower(3, 7); //21
+		laserTower tower3 = new laserTower(8, 5); //40
+		laserTower tower4 = new laserTower(2, 9); //18
+		laserTower tower5 = new laserTower(1, 7); //7
+		laserTower[] towers = new laserTower[]{tower1, tower2, tower3, tower4, tower5};
+		System.out.printf("Max number of troops: %d", maxTroops(towers));
 	}
 
 }
--- sorting/src/sorting/missionThree.java
+++ sorting/src/sorting/missionThree.java
@@ -10,13 +10,52 @@ package sorting;
  */
 
 public class missionThree {
-	public static <T extends Comparable<? super T>> int merge(T[] person1, T[] person2) {
-		return 0;
+	public static <T extends Comparable<? super T>> int merge(T[] person1, T[] person2, T[] array) {
+		int count1 = 0;
+		int count2 = 0;
+		int difference = 0;
+		int i = 0;
+		int j = 0;
+		int k = 0;
+		while(i < person1.length && j < person2.length) {
+			if((int)person1[i] < (int)person2[j]) {
+				array[k] = person1[i];
+				count1 = count1 + (int)person1[i];
+				k++;
+				i++;
+			}
+			else {
+				array[k] = person2[j];
+				count2 = count2 + (int)person2[j];
+				k++;
+				j++;
+			}
+		}
+		while(i < person1.length) {
+			array[k] = person1[i];
+			count1 = count1 + (int)person1[i];
+			k++;
+			i++;
+		}
+		while(j < person2.length) {
+			array[k] = person2[j];
+			count2 = count2 + (int)person2[j];
+			k++;
+			j++;
+		}
+		difference = Math.abs(count1 - count2);
+		return difference;
 	}
 	
 	public static void main(String[] args) {
 		Integer[] array1 = {1,3,5,7};
 		Integer[] array2 = {2,4,6};
+		Integer[] arrayMergeSorted = new Integer[array1.length + array2.length];
+		int difference = merge(array1, array2, arrayMergeSorted);
+		for(int i = 0; i < arrayMergeSorted.length; i++) {
+			System.out.printf("%d, ", arrayMergeSorted[i]);
+		}
+		System.out.printf("\nDifference of score is %d", difference);
 	}
 
 }
